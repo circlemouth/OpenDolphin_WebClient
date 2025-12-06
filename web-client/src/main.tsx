4a25a340c20263fbaa37ae54e941e7e7878c4120
circlemouth <circlemouth.h@gmail.com>
2025-12-07 00:21:19 +0900

    feat: add login app shell routing to reception and charts demos

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { LoginScreen } from './LoginScreen';
-import { OutpatientMockPage } from './features/outpatient/OutpatientMockPage';
+import { AppRouter } from './AppRouter';
 import './styles/global.css';
-
-const path = window.location.pathname;
-const App = path.startsWith('/outpatient-mock') ? OutpatientMockPage : LoginScreen;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 );
