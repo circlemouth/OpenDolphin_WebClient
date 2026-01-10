3f744e6651289e4c299ac26660a67db5ddbd9326
circlemouth <circlemouth.h@gmail.com>
2026-01-11 05:34:34 +0900

    Align Jackson deps and soften visit list failure

--- server-modernized/src/main/java/open/dolphin/rest/AppointmentOutpatientResource.java
+++ server-modernized/src/main/java/open/dolphin/rest/AppointmentOutpatientResource.java
@@ -128,7 +128,12 @@ public class AppointmentOutpatientResource extends AbstractOrcaWrapperResource {
 
         try {
             OrcaAppointmentListResponse listResponse = wrapperService.getAppointmentList(listRequest);
-            VisitPatientListResponse visitResponse = wrapperService.getVisitList(visitRequest);
+            VisitPatientListResponse visitResponse = null;
+            try {
+                visitResponse = wrapperService.getVisitList(visitRequest);
+            } catch (RuntimeException ex) {
+                details.put("visitListError", ex.getMessage());
+            }
 
             AppointmentOutpatientResponse response = new AppointmentOutpatientResponse();
             response.setRunId(runId);
@@ -152,6 +157,9 @@ public class AppointmentOutpatientResource extends AbstractOrcaWrapperResource {
             if (visitResponse != null) {
                 response.setVisitDate(visitResponse.getVisitDate());
                 response.setVisits(visitResponse.getVisits());
+            } else {
+                response.setVisitDate(appointmentDate.toString());
+                response.setVisits(List.of());
             }
 
             int slotCount = listResponse != null ? listResponse.getSlots().size() : 0;
