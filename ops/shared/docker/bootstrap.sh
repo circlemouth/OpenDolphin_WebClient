17c46e83f21582573b5c1df51becef2175ffd341
circlemouth <circlemouth.h@gmail.com>
2026-01-11 06:12:24 +0900

    Add schema bootstrap tool for modernized DB

--- ops/shared/docker/bootstrap.sh
+++ ops/shared/docker/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+if [[ -n "${OPENDOLPHIN_SCHEMA_ACTION:-}" ]] && [[ "${OPENDOLPHIN_SCHEMA_ACTION}" != "none" ]]; then
+    export JAVA_OPTS_APPEND="${JAVA_OPTS_APPEND:-} -Dhibernate.hbm2ddl.auto=${OPENDOLPHIN_SCHEMA_ACTION} -Djakarta.persistence.schema-generation.database.action=${OPENDOLPHIN_SCHEMA_ACTION}"
+fi
+
 if [[ $# -gt 0 && $1 == "$JBOSS_HOME/bin/standalone.sh" ]]; then
     shift
 fi
